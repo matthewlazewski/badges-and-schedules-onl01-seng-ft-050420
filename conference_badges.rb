532ceb73f079b0b5e10c48c7de3e8cd897145af5
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-05 18:51:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array.each do |name|
-    puts "Hello, my name is #{name}."
-  end 
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each { |name|  badges << "Hello, my name is #{name}." }
+  badges 
 end
