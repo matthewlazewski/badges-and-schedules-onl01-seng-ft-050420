5d628d0eced567665d60672e8be5551dc3d952fa
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-05 19:32:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(attendees)
 end 
 
 def printer(attendees)
-  attendees.each do |attendeee| 
+  attendees.each do |attendee| 
     puts batch_badge_creator(attendee)
     puts assign_rooms(attendee)
   end 
